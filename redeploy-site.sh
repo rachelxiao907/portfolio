6901ab06f2e802110227d6ce1f83b3fe9d9506a8
Rachel Xiao <60198727+rachelxiao907@users.noreply.github.com>
2023-02-26 14:54:56 -0800

    Creating Service - Part 2

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 
-# Kill existing tmux sessions
-tmux kill-session -a
+# Change to project directory
+cd portfolio
 
 # Update to latest git version
 git fetch && git reset origin/main --hard
 
 # Activate virtual environment and install dependencies
-source env/bin/activate
-pip install -r requirements.txt
+source portfolio/env/bin/activate
+pip install -r portfolio/requirements.txt
 
-# Start new detached tmux session
-tmux new-session -d -s flask_server
-tmux send-keys "cd portfolio" C-m
-tmux send-keys "source env/bin/activate" C-m
-tmux send-keys "flask run --host=0.0.0.0" C-m
-
-echo "Flask server started"
+# Restart myportfolio service
+systemctl restart myportfolio
