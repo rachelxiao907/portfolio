877006206029b894b0caa4056e85c0561b74e18d
Rachel Xiao <rachelxiao907@gmail.com>
2023-03-14 16:24:13 -0700

    added db

--- app/__init__.py
+++ app/__init__.py
@@ -3,8 +3,19 @@ import json
 import os
 
 from flask import Flask, render_template, request
+from peewee import *
+import datetime
+from playhouse.shortcuts import model_to_dict 
 
 app = Flask(__name__)
+
+mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"),
+        user=os.getenv("MYSQL_USER"),
+        password=os.getenv("MYSQL_PASSWORD"),
+        host=os.getenv("MYSQL_HOST"),
+        port=3306
+)
+print(mydb)
 
 common = {
     'first_name': 'Rachel',
