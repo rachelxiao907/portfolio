ddab2a57eb1a1e566fe3bcb0a1f6cf9365114f66
Rachel Xiao <rachelxiao907@gmail.com>
2023-04-09 13:26:25 -0700

    redeploy script update with docker

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -6,8 +6,7 @@ cd portfolio
 # Update to latest git version
 git fetch && git reset origin/main --hard
 
-# Spin containers down to prevent out of memory issues on our VPS 
-instances
+# Spin containers down to prevent out of memory issues on our VPS instances
 docker compose -f docker-compose.prod.yml down
 
 docker compose -f docker-compose.prod.yml up -d --build
